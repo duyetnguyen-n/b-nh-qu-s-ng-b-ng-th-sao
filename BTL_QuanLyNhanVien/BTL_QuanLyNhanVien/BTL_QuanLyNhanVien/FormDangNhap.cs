779450f62853000a61c305e1e1789b8907696d09
duyetnguyen-n <104747808+duyetnguyen-n@users.noreply.github.com>
2022-05-02 11:38:12 +0700

    Add files via upload

---
+++ BTL_QuanLyNhanVien/BTL_QuanLyNhanVien/BTL_QuanLyNhanVien/FormDangNhap.cs
@@ -0,0 +1,39 @@
+﻿using BTL_QuanLyNhanVien.AccountDao;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTL_QuanLyNhanVien
+{
+    public partial class FormDangNhap : Form
+    {
+        public FormDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+       
+
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void FormDangNhap_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
